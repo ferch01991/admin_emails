6a82c07cc6e69b7ef58f3bde24ff5cfeb3b07e43
Fernando Herrera <faherrera2@gmail.com>
2021-03-03 08:48:05 -0500

    Crear correo y listar los mismos

--- mails/forms.py
+++ mails/forms.py
@@ -1,5 +1,23 @@
 from django.forms import ModelForm
 
+from .models import Mail
+
 class CreateMailForm(ModelForm):
     class Meta:
-        
+        model = Mail
+        fileds = ['subject', 'content']
+        exclude = ['created_at']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['subject'].widget.attrs.update( {
+            'id': 'subject',
+            'class': 'form-control',
+            'placeholder': 'Subject'
+        } )
+
+        self.fields['content'].widget.attrs.update( {
+            'id': 'content',
+            'class': 'form-control',
+        } )
--- mails/views.py
+++ mails/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import reverse
 
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from .models import Mail
+
+from .forms import CreateMailForm
+
 # Create your views here.
 class MailListView(ListView):
     model = Mail
@@ -12,4 +16,7 @@ class MailListView(ListView):
 class MailCreateView(CreateView):
     model = Mail
     template_name = 'mails/create.html'
-    form_class
+    form_class = CreateMailForm
+
+    def get_success_url(self):
+        return reverse('mails:list')
