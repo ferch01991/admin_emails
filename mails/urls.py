1bcd09491e1a4059d1566aa7aebddef0fb31057b
Fernando Herrera <faherrera2@gmail.com>
2021-03-04 14:37:42 -0500

    Eliminar y editar correos

--- mails/urls.py
+++ mails/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import MailListView
 from .views import MailCreateView
+from .views import MailDeleteView
+from .views import MailUpdateView
 
 app_name = 'mails'
 
 urlpatterns = [
     path('', MailListView.as_view(), name='list'), #convertir una clase a una vista
     path('create', MailCreateView.as_view(), name='create'),
+    path('update/<int:pk>', MailUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', MailDeleteView.as_view(), name='delete'),
 ]
--- mails/views.py
+++ mails/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import reverse
 
+from django.urls import reverse_lazy
+
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import DeleteView
+from django.views.generic.edit import UpdateView
+
 from .models import Mail
 
 from .forms import CreateMailForm
@@ -17,6 +22,18 @@ class MailCreateView(CreateView):
     model = Mail
     template_name = 'mails/create.html'
     form_class = CreateMailForm
+
+    def get_success_url(self):
+        return reverse('mails:list')
+
+class MailDeleteView(DeleteView):
+    model = Mail
+    success_url = reverse_lazy('mails:list')
+
+class MailUpdateView(UpdateView):
+    model = Mail
+    form_class = CreateMailForm
+    template_name = 'mails/update.html'
 
     def get_success_url(self):
         return reverse('mails:list')
